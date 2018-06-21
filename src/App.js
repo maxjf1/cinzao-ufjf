4dc1e7b1450f99ee9c4c1537d0284ff7050b1fce
maxjf1 <maxwellsouzacarvalho@gmail.com>
2018-06-21 16:20:01 -0300

    added base project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { blueGrey, red } from '@material-ui/core/colors';
+import { Button } from '@material-ui/core';
+import "typeface-roboto"
+
+const theme = createMuiTheme({
+    palette: {
+        primary: blueGrey,
+        secondary: red
+    },
+});
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <MuiThemeProvider theme={theme}>
+                <Button variant="contained" color="primary">
+                    Primary
+                </Button>
+                <Button variant="contained" color="secondary">
+                    Secondary
+                </Button>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
